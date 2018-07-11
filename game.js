a65598f5f8f69cc6208b05b920dbbee1eb135279
Gabriel Lomba <gabriellomba@hotmail.com>
2018-07-10 21:47:58 -0300

    Connect the play again buttons to the start game logic and show highest scores div when the button is pressed

--- game.js
+++ game.js
@@ -1,31 +1,45 @@
 'use strict'
-const startButton = document.getElementById("startGameBtn");
-const food = document.getElementById("food");
+
+const cover = document.querySelector('.cover');
+const startButton = document.getElementById('startGameBtn');
+const highestScoresBox = document.querySelector('.highest-scores-box');
+const highestScoresBtn = document.getElementById('highestScoresBtn');
+const playAgainBtns = document.querySelectorAll('.again');
+const food = document.getElementById('food');
 const width = window.innerWidth;
 const height = window.innerHeight;
 console.log(height);
 console.log(width);
-startButton.addEventListener("click", startgame);
 
-function startgame(){
-  // document.getElementById("beginning-modal").style.display = 'none';
-  var foodWidth = randomGen(0,width,10);
-  foodWidth = foodWidth+"px";
-  var foodHeight = randomGen(0,height,10);
-  foodHeight = foodHeight+"px";
-  food.style.top = foodHeight;
-  food.style.left = foodWidth;
+function hideCover () {
+  cover.style.display = 'none';
 }
 
+function hideCoverChildren () {
+  Array.from(cover.children).forEach(child => {
+    child.style.display = 'none';
+  });
+}
+
+function startgame() {
+  hideCover();
+  hideCoverChildren();
+  const foodLeft = `${randomGen(0,width,10)}px`;
+  const foodTop = `${randomGen(0,height,10)}px`;
+  food.style.top = foodTop;
+  food.style.left = foodLeft;
+}
+
+function showHighestScores () {
+  hideCoverChildren();
+  highestScoresBox.style.display = 'grid';
+}
 
 function randomGen(min, max, div){
-  var res = Math.floor(Math.random() * (max - min + 1)) + min;
-  // for(var i = res; i < res + div; i++)
-  // {
-  //   if( i % div == 0 )
-  //   {
-  //     return i;
-  //   }
-  // } console.log("error");
-  return (res + (div - res % div));
+  const res = Math.floor(Math.random() * (max - min + 1)) + min;
+  return res + (div - res % div);
 }
+
+startButton.addEventListener('click', startgame);
+playAgainBtns.forEach(btn => btn.addEventListener('click', startgame))
+highestScoresBtn.addEventListener('click', showHighestScores)
