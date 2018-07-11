2a97013ef23d918f8c2566bde1efaf664e0370e1
Daniel Aguilera <aguilera@Daniels-MacBook-Pro.local>
2018-07-10 19:04:18 -0500

    change the logic of the randomgen function

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ console.log(width);
 startButton.addEventListener("click", startgame);
 
 function startgame(){
-  document.getElementById("beginning-modal").style.display = 'none';
+  // document.getElementById("beginning-modal").style.display = 'none';
   var foodWidth = randomGen(0,width,10);
   foodWidth = foodWidth+"px";
   var foodHeight = randomGen(0,height,10);
@@ -20,11 +20,12 @@ function startgame(){
 
 function randomGen(min, max, div){
   var res = Math.floor(Math.random() * (max - min + 1)) + min;
-  for(var i = res; i < res + div; i++)
-  {
-    if( i % div == 0 )
-    {
-      return i;
-    }
-  } console.log("error");
+  // for(var i = res; i < res + div; i++)
+  // {
+  //   if( i % div == 0 )
+  //   {
+  //     return i;
+  //   }
+  // } console.log("error");
+  return (res + (div - res % div));
 }
