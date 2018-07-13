3b3a8de73fd0a52577e7bd39c4e88a135e324fbd
Rebjai <lordfenix001@hotmail.com>
2018-07-12 22:34:31 -0500

    made showPlayAgain()

--- game.js
+++ game.js
@@ -2,19 +2,35 @@ import state from './store.js'
 import { updateSnake, resetSnake } from './snake.js'
 import { generateRandomFood } from './food.js'
 
-const cover = document.querySelector('.cover');
-const startButton = document.getElementById('startGameBtn');
-const highestScoresBox = document.querySelector('.highest-scores-box');
-const highestScoresBtn = document.getElementById('highestScoresBtn');
+const $ = (sel) => {return document.querySelector(sel)}
+const cover = $('.cover');
+const startButton = $('#startGameBtn');
+const highestScoresBox = $('.highest-scores-box');
+const highestScoresBtn = $('#highestScoresBtn');
+const playAgainBox = $('.play-again-box')
 const playAgainBtns = document.querySelectorAll('.again');
+
 
 function hideCover () {
   cover.style.display = 'none';
 }
-
+function showCover () {
+  cover.style.display = 'block';
+}
+function showPlayAgain () {
+  showCover()
+  playAgainBox.style.display = 'grid';
+  showChildren(playAgainBox);
+}
+// showPlayAgain()
 function hideCoverChildren () {
   Array.from(cover.children).forEach(child => {
     child.style.display = 'none';
+  });
+}
+function showChildren (el) {
+  Array.from(el.children).forEach(child => {
+    child.style.display = 'block';
   });
 }
 
@@ -24,6 +40,7 @@ function startgame() {
   generateRandomFood()
   state.GAME_HAS_STARTED = true
   return false;
+  
 }
 
 function showHighestScores () {
